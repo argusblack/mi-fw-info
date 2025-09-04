1276322392940237a3e2148f6a2015a38c0d87ca
argusblack <rubixduke@gmail.com>
2025-09-04 13:26:37 +0200

    add navee links, update app

--- app.py
+++ app.py
@@ -16,9 +16,8 @@ def convert_to_utc(timestamp):
     utc_time = datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
     return utc_time
 
-
-st.title("Mi Firmware Info")
-st.text(f"Snapshot: {convert_to_utc(modification_date)}")
+st.title("Mi|Navee Firmware Info")
+st.write(f"Snapshot: {convert_to_utc(modification_date)}")
 
 # Extract models for the dropdown menu
 names = {item['name']: item['model'] for item in data}
@@ -26,32 +25,60 @@ names = {item['name']: item['model'] for item in data}
 # Create a dropdown menu for selecting the model
 name_selected = st.selectbox("**Select Model**", sorted(names.keys()))
 
+st.write("**Caution:** Make sure to select the correct model by comparing the **Model ID**! Use tool like *nRF Connect* to find out the correct **Model ID** of your device.")
+
 # Display the corresponding data based on the selected model
 if name_selected:
     model_selected = names[name_selected]
     selected_data = next(item for item in data if item['model'] == model_selected)
 
-    st.header(name_selected)
-    st.subheader('General')
-    st.write(f"**Model ID**: {model_selected}")
-    st.write(f"**Change Date (UTC)**: {convert_to_utc(selected_data['firmware']['upload_time'])}")
-    if selected_data['firmware']['changeLog']:
-        st.write(f"**Change Log**: {selected_data['firmware']['changeLog']}")
+    st.markdown(f"<h1 style='text-align: center;'>{name_selected}</h1>", unsafe_allow_html=True)
 
-    st.subheader('Firmware Details')
-    st.write(f"**Version**: {selected_data['firmware']['version']}")
-    st.write(f"**MD5**: {selected_data['firmware']['md5']}")
-    st.write(f"**Update URL**: [Download Firmware]({selected_data['firmware']['safe_url']})")
+    col1, col2 = st.columns(2)
+    with col1:
+        if 'extra' in selected_data:
+            img = selected_data['extra']['imageMin']
+            st.image(img, width=200)
+            st.write(f"Image source: {img.split('?')[0]}")
 
-    if 'mcu_md5' in selected_data['firmware']:
-        st.subheader('MCU Firmware Details')
-        st.write(f"**Version**: {selected_data['firmware']['mcu_version']}")
-        st.write(f"**MD5**: {selected_data['firmware']['mcu_md5']}")
-        st.write(f"**Update URL**: [Download MCU Firmware]({selected_data['firmware']['mcu_safe_url']})")
+    with col2:
+        st.write(f"**Model ID**: {model_selected}")
+        date = selected_data['firmware'].get('upload_time', 0)
+        if date != 0:
+            st.write(f"**Change Date (UTC)**: {convert_to_utc(date)}")
+        if selected_data['firmware']['changeLog']:
+            st.write(f"**Change Log**: {selected_data['firmware']['changeLog']}")
+
+        if 'version' in selected_data['firmware']:
+            md5 = selected_data['firmware'].get('md5')
+
+            st.subheader('BLE Firmware Details')
+            st.write(f"**Version**: {selected_data['firmware']['version']}")
+            if md5:
+                st.write(f"**MD5**: {md5}")
+            st.write(f"**Update URL**: [Download Firmware]({selected_data['firmware']['safe_url']})")
+
+        if 'mcu_version' in selected_data['firmware']:
+            md5 = selected_data['firmware'].get('mcu_md5')
+
+            st.subheader('MCU Firmware Details')
+            st.write(f"**Version**: {selected_data['firmware']['mcu_version']}")
+            if md5:
+                st.write(f"**MD5**: {md5}")
+            st.write(f"**Update URL**: [Download MCU Firmware]({selected_data['firmware']['mcu_safe_url']})")
+
+        if 'bms_version' in selected_data['firmware']:
+            md5 = selected_data['firmware'].get('bms_md5')
+
+            st.subheader('BMS Firmware Details')
+            st.write(f"**Version**: {selected_data['firmware']['bms_version']}")
+            if md5:
+                st.write(f"**MD5**: {md5}")
+            st.write(f"**Update URL**: [Download bms Firmware]({selected_data['firmware']['bms_safe_url']})")
 
 
 st.header("Disclaimer")
-st.write("""The links to firmware files provided on this site are sourced directly from content delivery network of the respective manufacturers. These links are intended for informational purposes only and are not hosted or controlled by us. We do not claim ownership of the firmware files nor do we endorse or guarantee their content, functionality, or safety.
+st.write("""All product images links provided on this site are sourced directly from content delivery network of the respective manufacturers. These images and links are intended for informational purposes only and are not hosted or controlled by us. We do not claim ownership of any file nor do we endorse or guarantee their content, functionality, or safety.
 
  1. Purpose and Intent:
 The links provided on this website lead to firmware downloads for various devices. The purpose of sharing these links is to facilitate access to firmware updates that may be necessary for maintaining or upgrading your devices. These links are intended for informational and educational purposes only.
@@ -63,7 +90,7 @@ We do not endorse or guarantee the accuracy, legality, or safety of the firmware
 Please be aware that the use and distribution of firmware may be governed by the terms and conditions set forth by the respective manufacturers. Users are advised to review and adhere to these terms before downloading or using any firmware.
 
  4. Intellectual Property Rights:
-The firmware files linked from this site are the property of their respective copyright holders. By providing these links, we do not claim any ownership or rights to the firmware. Any use of the firmware must comply with the copyright and licensing terms specified by the manufacturers.
+The product images and firmware files linked from this site are the property of their respective copyright holders. By providing these links, we do not claim any ownership or rights. Any use of the data must comply with the copyright and licensing terms specified by the manufacturers.
 
  5. No Liability:
 We are not liable for any direct or indirect damages or losses resulting from the use of the firmware or the links provided. Users assume full responsibility for their use of the linked firmware and for ensuring compliance with any relevant laws and manufacturer guidelines.
